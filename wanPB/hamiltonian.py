140da34da0c92c2122d2543b268b6b4a551222be
Chengcheng-Xiao <iconxicon@me.com>
2021-07-04 21:53:02 +0100

    add progressbar.

--- wanPB/hamiltonian.py
+++ wanPB/hamiltonian.py
@@ -1,8 +1,25 @@
+import sys
 import numpy as np
 from wanPB.w90_parser import *
 import pybinding as pb
 import matplotlib.pyplot as plt
 pb.pltutils.use_style()
+
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    '''
+    progress bar function from https://stackoverflow.com/a/34482761/12660859
+    '''
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
 
 def gen_lat(seedname="wannier90"):
     '''
@@ -12,22 +29,26 @@ def gen_lat(seedname="wannier90"):
     lat_vec, Rlatt, hopps, deg, num_wann, num_kpts = read_tb(Filename=seedname+"_tb.dat")
     wan_centers = read_center(Filename=seedname+"_centres.xyz")
 
-    print(f"Converting... \t\t\t\t", end='',flush=True)
+    #print(f"Converting fromat: \t\t\t\t",flush=True)
 
     # generate lat object
     lat = pb.Lattice(a1 = lat_vec[0],
                      a2 = lat_vec[1],
                      a3 = lat_vec[2])
 
-    # icount1=0
-    for i in range(len(Rlatt)):
+    # putting in all sites and their on-site energies.
+    for i in progressbar(range(len(Rlatt)), "Converting on-site energyies: ", 20):
         if all(Rlatt[i]==[0, 0, 0]):
             for m in range(num_wann):
                 # icount1+=1
                 lat.add_one_sublattice(str(m), wan_centers[m], onsite_energy=np.real(hopps[i,m,m]))
 
-    # icount = 0
-    for i in range(len(Rlatt)):
+    # putting in all hopping elements.
+    # NOTE: pybinding reports error when adding hermitian conjugate of existing hoppings.
+    # we just do brute force error handling on this since it's faster that using if
+    # to determine which hoppings to add. (like in pythtb's interfce.)
+    # note that this means we assume Wannier Hamiltonian is Hermitian.
+    for i in progressbar(range(len(Rlatt)), "Converting hopping energyies: ", 20):
         for m in range(num_wann):
             for n in range(num_wann):
                 try:
@@ -35,10 +56,10 @@ def gen_lat(seedname="wannier90"):
                                         str(m),
                                         str(n),
                                         hopps[i,m,n])
-                    # icount+=1
                 except:
                     continue
 
-    print("done.",flush=True)
+
+    #print("done.",flush=True)
 
     return lat
